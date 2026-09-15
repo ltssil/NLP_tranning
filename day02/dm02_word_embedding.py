8c2d683a442509a2a806e7022a07c278a3e9d40a
ltssil <504605016@qq.com>
2026-09-15 09:51:37 +0800

    Word Embedding实现

---
+++ day02/dm02_word_embedding.py
@@ -0,0 +1,80 @@
+"""
+案例：
+    演示文件张量实现方式之 word Embedding
+
+大白话解释：word2vec 和 word Embedding区别
+    word2vec :
+        先办证，后干活，会先训练出词向量(模型)，后续再代入模型进行其他操作
+    word Embedding :
+        边办证，边干活，训练过程中，词向量会自动生成 -> RNN的词嵌入层
+"""
+
+# 导包
+import torch                                                    # 深度学习框架，封装了和张量相关的各种操作
+from tensorflow.keras.preprocessing.text import Tokenizer       # 词汇映射器
+from torch.utils.tensorboard import SummaryWriter               # 可视化词向量
+import jieba                                                    # 分词器
+import torch.nn as nn                                           # 神经网络模块
+
+# todo 1. 定义函数，演示：word Embedding 将文本转成词向量，并可视化
+def dm01_embedding_show() :
+    # 1. 定义变量，记录待处理的文本
+    sentence1 = '传智教育是一家上市公司，旗下有黑马程序员品牌。我是在黑马这里学习人工智能'
+    sentence2 = "我爱自然语言处理"
+
+    # 2. 把上述的两句话，封装成列表
+    sentences = [sentence1, sentence2]
+
+    # 3. 使用jieba进行分词处理
+    # 3.1 定义变量: 记录 分词后的数据 -> 词语列表
+    word_list = []
+
+    # 3.2 遍历上述的句子列表，获取到每个例子
+    for sentence in sentences :
+        # 3.3 使用jieba进行分词处理，并将分词结果添加到 word_list 中
+        word_list.append(jieba.lcut(sentence))
+    """
+        [
+            ['传智', '教育', '是', '一家', '上市公司', '，', '旗下', '有', '黑马', '程序员', '品牌', '。', '我', '是', '在', '黑马', '这里', '学习', '人工智能'], 
+            ['我', '爱', '自然语言', '处理']
+        ]
+    """
+    # 3.4 打印分词结果
+    print(word_list)
+
+    # 4. 构建词汇表，进行 文本数值化（词向量）
+    # 4.1 初始化词汇映射器
+    my_tokenizer = Tokenizer()
+
+    # 4.2 拟合训练数据，统计词频，并构建：词汇表
+    my_tokenizer.fit_on_texts(word_list)
+    """{'是': 1, '黑马': 2, '我': 3, '传智': 4, '教育': 5, '一家': 6, '上市公司': 7, '，': 8, '旗下': 9, '有': 10, '程序员': 11, '品牌': 12, '。': 13, '在': 14, '这里': 15, '学习': 16, '人工智能': 17, '爱': 18, '自然语言': 19, '处理': 20}"""
+
+    # 4.3 查看 词 和 索引 之间的映射关系
+    print(my_tokenizer.word_index)
+
+    # 4.4 获取去重后的所有词汇列表
+    my_token_list = my_tokenizer.word_index.values()
+    print(my_token_list)
+    """dict_values([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])"""
+
+    # 4.5 将分词后的文本 转成 数字序列
+    seq2id = my_tokenizer.texts_to_sequences(word_list)
+    print(f'文本转成数字序列:{seq2id}')
+    """文本转成数字序列:[[4, 5, 1, 6, 7, 8, 9, 10, 2, 11, 12, 13, 3, 1, 14, 2, 15, 16, 17], [3, 18, 19, 20]]"""
+
+    # 5. 创建 词嵌入层， 把文本（即：词对应的编号）转成词向量
+    # 5.1 创建 词嵌入层 对象
+    """
+        :param1 词汇表大小，即：唯一的词个数
+        :param2 词向量的维度
+    """
+    embed = nn.Embedding(num_embeddings=len(my_token_list), embedding_dim=8)
+
+    # 5.2 查看 词嵌入层的权重参数（即：词向量)
+    print(f'embed: {embed.weight.data}' )
+    print(f'embed.shape: {embed.weight.data.shape}' )       # torch.Size([20,8])
+
+# todo 2. 测试代码
+if __name__ == '__main__':
+    dm01_embedding_show()
